2de362cdd9a510b035afc8f5e992b992c573df7d
yuce-haydar <haydar8w@gmail.com>
2025-07-18 00:29:44 +0300

    son çalışır

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
-use App\Models\HomePageSetting;
+use App\Models\HomePageSetting; 
 use App\Models\AboutPageSetting;
 use App\Models\Facility;
 use App\Models\Project;
